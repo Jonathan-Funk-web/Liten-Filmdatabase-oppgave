f8d14a76469b79c7a4ecda61988e89ef86edae6a
Jonathan Torp <Jonathan.Torp@funkweb.org>
2025-03-03 11:09:39 +0100

    Lagt til vis_filmer()

--- filmdatabase.py
+++ filmdatabase.py
@@ -1,6 +1,6 @@
 filmer = []
 
-def legg_til_film(tittel: str = "Tittel Mangler", regissør: str ="Regissør Mangler", produsent: str ="Produsent Mangler", år: int = 0, sjanger: list[str] = ["Sjanger(e) Mangler"]) -> dict:
+def legg_til_film(tittel: str = "Tittel Mangler", regissør: str ="Regissør Mangler", produsent: str ="Produsent Mangler", år: int = 0, sjanger: list[str] = ["Sjanger(e) Mangler"]) -> None:
     """Tar de forselige detaljene til filmen og lagrer de som en ordbok i `filmer` listen.
     Args:
         tittel (str, optional): Tittelen til filmen. Hvis ingen verdi er oppgit så er standardverdien `"Tittel Mangler"`.
@@ -11,10 +11,24 @@ def legg_til_film(tittel: str = "Tittel Mangler", regissør: str ="Regissør Man
     Returns:
         dict: Slår sammen all infoen in til en ordbok.
     """
+    
+    #TODO: Jeg kan legge til noe kode som gjør at denne funksjonen sier ifra hvis det er noen av argumentene som mangler.
     filmer.append({"Tittel":tittel, "Regissør":regissør, "Produsent":produsent, "År":år, "Sjanger":sjanger})
-    return 
+    
+    print("Film lagt til i `filmer` listen.")
+    
+    return None
 
-print(filmer)
-legg_til_film("Titeline","Regisman","Produsentus",2001,["Action","Drama"])
-legg_til_film()
-print(filmer[1])
+def vis_filmer() -> list[dict] | int:
+    """Denne funksjonen skriver ut alle filmene i databasen, enn sålenge så er det bare `filmer` listen.
+
+    Returns:
+        list[dict]: Den lille film databasen vår.
+        int: Verdien er `0` hvis databasen er tom, ellers så er verdien hvor mange filmer som er i databasen.
+    """
+    if len(filmer) == 0:
+        print("Det er ingen filmer i databasen.")
+        return 0
+    for film in filmer:
+        print(film)
+        return len(filmer)
